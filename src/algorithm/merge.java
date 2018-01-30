83b70e47dbecdec7d04989175b4df66680a0287d
damaomao <damaomao@10.191.14.224>
2018-01-31 00:54:27 +0800

    remove sneaker

--- src/algorithm/merge.java
+++ src/algorithm/merge.java
@@ -12,46 +12,45 @@ package algorithm;
 public class merge {
     public static void sort(int[] a){
         int[] temp = new int[a.length];
-        MergeSort(a, 0, a.length-1, temp);
-        for(int b: temp){
+        MergeSort(a, 0, a.length-1);
+        for(int b: a){
             System.out.print(b + ",");
         }
         System.out.println("");
     }
-    private static void MergeSort(int[] a, int first, int last, int[] temp){
+    private static void MergeSort(int[] a, int first, int last){
         if(first < last){
             int mid = (first + last)/2;
-            MergeSort(a, first, mid, temp);
-            MergeSort(a, mid+1, last, temp);
-            Merge(a, first, last, temp);
+            MergeSort(a, first, mid);
+            MergeSort(a, mid+1, last);
+            Merge(a, first, last);
         }       
     }
-    private static void Merge(int[] a,int first, int last, int[] temp){
-        int mid = (first + last)/2;
-        final int  MIDDLE = mid;
+    private static void Merge(int[] a,int first, int last){
+        int[] temp = new int[last-first+1];
+        int mid = (first + last)/2+1;
+        final int MIDDLE = mid-1;
         final int FIRST = first;
         int i=0;
-        while(first <= MIDDLE && (mid+1) <= last){
-            if(a[first] <= a[mid+1]) {
-                temp[i] = a[first];
-                i++;
-                first++;
+        while(first <= MIDDLE && mid <= last){
+            if(a[first] <= a[mid]) {
+                temp[i++] = a[first++];
+                
             }
             else{
-                temp[i] = a[mid+1];
-                i++;
-                mid++;
+                temp[i++] = a[mid++];
+               
+               
             }
         }
         while(first <= MIDDLE){
-            temp[i] = a[first];
-                i++;
-                first++;
+            temp[i++] = a[first++];
+               
         }
-        while((mid+1) <= last){
-            temp[i] = a[mid+1];
-            i++;
-            mid++;
+        while(mid <= last){
+            temp[i++] = a[mid++];
+            
+          
         }
         for(int j=0;j<i;j++){
             a[FIRST + j] = temp[j];
