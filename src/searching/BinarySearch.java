7b954ce447002d2ba4b59e47836a6320bf407d9c
yueliliu <yueliliu@YUELILIU-CN.oradev.oraclecorp.com>
2018-01-31 15:37:28 +0800

    add binary search

---
+++ src/searching/BinarySearch.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package searching;
+
+/**
+ *
+ * @author yueliliu
+ */
+public class BinarySearch {
+    public static boolean search(int a[], int target){
+        sorting.swap.QuickSort(a);
+        int len = a.length;
+        int temp = len/2;
+        return false;
+    }
+}
